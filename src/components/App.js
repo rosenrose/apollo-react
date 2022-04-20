68068d8b8e23896f6dddcbbb66bdb0c2f18cffc0
rosenrose <crazytempler@gmail.com>
2022-04-20 13:28:42 +0900

    gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Home from "../routes/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Detail />} />
