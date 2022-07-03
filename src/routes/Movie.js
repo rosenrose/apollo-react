5e80b09842d495d0bd2e8ad4d13f4f59855b55ab
rosenrose <crazytempler@gmail.com>
2022-07-03 11:45:31 +0900

    styles

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,18 +1,52 @@
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query ($movieId: String!) {
     movie(id: $movieId) {
-      items {
-        thumbnailUrl
-        snippet {
-          title
-          description
-        }
+      id
+      thumbnailUrl
+      snippet {
+        title
+        description
       }
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 1rem;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+  margin-bottom: 1rem;
+`;
+
+const Description = styled.p`
+  font-size: 1.7rem;
+`;
+
+const Image = styled.div`
+  width: 40%;
+  height: 50%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 0.5rem;
 `;
 
 const Movie = () => {
@@ -21,16 +55,19 @@ const Movie = () => {
   // console.log(data);
 
   return (
-    <>
+    <Container>
       {loading ? (
-        <h1>loading</h1>
+        "Loading..."
       ) : (
-        <figure>
-          <figcaption>{data.movie.items[0].snippet.title}</figcaption>
-          <img src={data.movie.items[0].thumbnailUrl} alt={data.movie.items[0].thumbnailUrl} />
-        </figure>
+        <>
+          <Column>
+            <Title>{data.movie.snippet.title}</Title>
+            <Description>{data.movie.snippet.description}</Description>
+          </Column>
+          <Image bg={data.movie.thumbnailUrl} />
+        </>
       )}
-    </>
+    </Container>
   );
 };
 
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,39 +1,112 @@
+import { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const ALL_MOVIES = gql`
-  query getMovies {
-    allMovies {
-      items {
-        snippet {
-          title
-        }
-        contentDetails {
-          videoId
-        }
-      }
+  query getMovies($playlistId: String!) {
+    allMovies(id: $playlistId) {
+      id
+      thumbnailUrl
     }
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+`;
+
+const Loading = styled.div`
+  font-size: 1.2rem;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 1rem;
+`;
+
+const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 1.5rem;
+  width: 80%;
+  position: relative;
+  top: -50px;
+`;
+
+const PosterContainer = styled.div`
+  height: 15rem;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background-color: transparent;
+`;
+
+const PosterBg = styled.div`
+  background-image: url(${(props) => props.background});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 0.5rem;
+`;
+
+const playlists = ["UUyWiQldYO_-yeLJC0j5oq2g", "PLtYQ7DpMMg-I22iICgrMNC1ln3ZT4FxUj"];
+
 const Movies = () => {
-  const { data, loading } = useQuery(ALL_MOVIES);
+  const [id, setId] = useState(playlists[0]);
+  const onChange = (event) => {
+    const value = event.target.value;
+    if (value.length) {
+      setId(value);
+    }
+  };
+
+  const { data, loading } = useQuery(ALL_MOVIES, { variables: { playlistId: id } });
   // console.log(data);
 
   return (
-    <>
-      {loading ? (
-        <h1>loading</h1>
-      ) : (
-        <ul>
-          {data.allMovies.items.map((movie) => (
-            <li key={movie.contentDetails.videoId}>
-              <Link to={`/movie/${movie.contentDetails.videoId}`}>{movie.snippet.title}</Link>
-            </li>
+    <Container>
+      <Header>
+        <Title>Apollo Movies</Title>
+        <input type="text" onChange={onChange} list="playlists" style={{ width: "20rem" }} />
+        <datalist id="playlists">
+          {playlists.map((id) => (
+            <option value={id} />
           ))}
-        </ul>
+        </datalist>
+      </Header>
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <MoviesGrid>
+          {data.allMovies.map((movie) => (
+            <PosterContainer key={movie.id}>
+              <Link to={`/movie/${movie.id}`}>
+                <PosterBg background={movie.thumbnailUrl} />
+              </Link>
+            </PosterContainer>
+          ))}
+        </MoviesGrid>
       )}
-    </>
+    </Container>
   );
 };
 
